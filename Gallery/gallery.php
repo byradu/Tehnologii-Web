440e06e98559d173cc0f7d27536562ad8b782245
Stelian Loghin <alexandruloghinstelian121@yahoo.com>
2020-05-21 12:54:21 +0300

    ADMIN can add photos, gallery uses data from DB

--- Gallery/gallery.php
+++ Gallery/gallery.php
@@ -1,5 +1,9 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,18 +12,65 @@
     <link rel="icon" type="image/png" href="..\logo.jpg">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
+
 <body>
     <header>
-        <h2>Numismatic Artefact Explorer - Gallery</h2>
+    <h2>Numismatic Artefact Explorer - Gallery</h2>
+        <?php
+        if (isset($_SESSION['username'])) {
+            if ($_SESSION['username'] == "admin") {
+                echo '<div class="gallery-upload"> 
+                <form action="../Includes/gallery-upload.php" method="POST" enctype="multipart/form-data">
+                    <input required type="text" name="filetitle" placeholder="File title">
+                    <input required type="number" name="valoare" placeholder="Value">
+                    <input required type="text" name="tara" placeholder="Country">
+                    <input required type="date" name="createdAt" placeholder="Perioada de emisie">
+                    <input required type="text" name="descriere" placeholder="Description">
+                    <input required type="file" name="file" placeholder="file">
+                    <button type="submit" name="submit">Upload</button>
+                </form>
+            </div>';
+                // echo'<h1>HAI ADMINE</h1>';\
+            }
+        }
+        ?>
+
     </header>
     <main>
         <div>
             <h3>Romania</h3>
+            
             <ul>
-                <li>
-                    <h5>5 Sutimi - Alexandru Ioan Cuza</h5>
+            <?php 
+                include_once '../Includes/connection.inc.php';
+                $sql="SELECT * FROM COINS";
+                $stmt=mysqli_stmt_init($conn);
+                if(!mysqli_stmt_prepare($stmt,$sql)){
+                    echo 'AFARA IN PLM';
+                }else{
+                    mysqli_stmt_execute($stmt);
+                    $result=mysqli_stmt_get_result($stmt);
+                    while($row=mysqli_fetch_assoc($result)){
+                        echo '<li style="text-align:center;background:rgba(255,255,255,0.4); color:black;border:1px solid black;margin:0.5em;">
+
+                        <img src="gallery/' . $row["imgFullName"] . '">
+                        <p>Title: ' .$row['title'] . '</p>
+                        <p>Value: ' .$row['value'] . '</p>
+                        <p>Country: ' .$row['country'] . '</p>
+                        <p>Created at: ' .$row['createdAt'] . '</p>
+                        <p>Description: ' .$row['description'] . '</p>
+                        </li>';
+                    }
+                }
+            ?>
+                <li style="text-align:center;background:rgba(255,255,255,0.4); color:black;border:1px solid black;border-radius:100deg;">
+                    <h5> Title: 5 Sutimi - Alexandru Ioan Cuza</h5>
                     <img src="https://en.numista.com/catalogue/photos/roumanie/5e5c5a15796da7.25898472-original.jpg" alt="5sute">
                     <img src="https://en.numista.com/catalogue/photos/roumanie/5e5c5a153fd816.84235286-original.jpg" alt="5sute2">
+                    <p>Value:NAN</p>
+                    <p>Country: Romania</p>
+                    <p>Created at: ?</p>
+                    <p>Description: o moneda</p>
                 </li>
                 <li>
                     <h5>10 Bani - Carol I</h5>
@@ -28,8 +79,8 @@
                 </li>
                 <li>
                     <h5>1 Leu</h5>
-                    <img  class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea31101efc3d8.38696812-original.jpg" alt="1leu1">
-                    <img  class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea3110227a1b0.92580680-original.jpg" alt="1leu1.2">
+                    <img class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea31101efc3d8.38696812-original.jpg" alt="1leu1">
+                    <img class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea3110227a1b0.92580680-original.jpg" alt="1leu1.2">
                 </li>
                 <li>
                     <h5>100 Lei - Mihai I</h5>
@@ -47,7 +98,7 @@
             <h3>China</h3>
             <ul>
                 <li>
-                <h5>5 Sutimi - Alexandru Ioan Cuza</h5>
+                    <h5>5 Sutimi - Alexandru Ioan Cuza</h5>
                     <img src="https://en.numista.com/catalogue/photos/roumanie/5e5c5a15796da7.25898472-original.jpg" alt="5sute">
                     <img src="https://en.numista.com/catalogue/photos/roumanie/5e5c5a153fd816.84235286-original.jpg" alt="5sute2">
                 </li>
@@ -58,8 +109,8 @@
                 </li>
                 <li>
                     <h5>1 Leu</h5>
-                    <img  class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea31101efc3d8.38696812-original.jpg" alt="1leu1">
-                    <img  class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea3110227a1b0.92580680-original.jpg" alt="1leu1.2">
+                    <img class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea31101efc3d8.38696812-original.jpg" alt="1leu1">
+                    <img class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea3110227a1b0.92580680-original.jpg" alt="1leu1.2">
                 </li>
                 <li>
                     <h5>100 Lei - Mihai I</h5>
@@ -77,7 +128,7 @@
             <h3>USA</h3>
             <ul>
                 <li>
-                <h5>5 Sutimi - Alexandru Ioan Cuza</h5>
+                    <h5>5 Sutimi - Alexandru Ioan Cuza</h5>
                     <img src="https://en.numista.com/catalogue/photos/roumanie/5e5c5a15796da7.25898472-original.jpg" alt="5sute">
                     <img src="https://en.numista.com/catalogue/photos/roumanie/5e5c5a153fd816.84235286-original.jpg" alt="5sute2">
                 </li>
@@ -88,8 +139,8 @@
                 </li>
                 <li>
                     <h5>1 Leu</h5>
-                    <img  class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea31101efc3d8.38696812-original.jpg" alt="1leu1">
-                    <img  class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea3110227a1b0.92580680-original.jpg" alt="1leu1.2">
+                    <img class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea31101efc3d8.38696812-original.jpg" alt="1leu1">
+                    <img class="bnc" src="https://en.numista.com/catalogue/photos/roumanie/5ea3110227a1b0.92580680-original.jpg" alt="1leu1.2">
                 </li>
                 <li>
                     <h5>100 Lei - Mihai I</h5>
@@ -104,6 +155,7 @@
             </ul>
         </div>
     </main>
-    
+
 </body>
+
 </html>
---
+++ Includes/gallery-upload.php
@@ -0,0 +1,57 @@
+<?php 
+    if(isset($_POST['submit'])){
+        $title=$_POST['filetitle'];
+        if(empty($title)){
+            $title="gallery";
+        }else{
+            $title=strtolower(str_replace(" ","-",$title));
+        }
+        $valoare=$_POST['valoare'];
+        $tara=$_POST['tara'];
+        $createdAt=$_POST['createdAt'];
+        $descriere=$_POST['descriere'];
+        // echo $title . "," .  $createdAt . "," . $valoare . "," . $tara . "," . $descriere;
+        //$_Files- array de iteme uploadate prin POST
+        $file=$_FILES['file'];
+        // luam datele despre fisierul uploadat
+        $fileName=$file["name"];
+        $fileType=$file["type"];
+        $fileTempName=$file["tmp_name"];
+        $fileError=$file["error"];
+        $fileSize=$file["size"];
+        // verificam extensia fisierului
+        $fileExt=explode(".",$fileName);
+        // ma duc la ultimul element din stringu splituit, pentru ca ultimele litere dupa . sunt de fapt extensia fisierului
+        $fileActualExt=strtolower(end($fileExt));
+        // array pentru extensiile permise
+        $allowed=array("jpg","jpeg","png");
+        if(in_array($fileActualExt,$allowed)){
+            if($fileError===0){
+                if($fileSize<2000000){
+                    // generez id unic ca sa nu fie mai multe imagini cu acelasi path
+                    $imageFullName=$title . "." .uniqid("",true) . "." . $fileActualExt;
+                    $fileDestination="../Gallery/gallery/" .$imageFullName;
+                    include_once "connection.inc.php";
+                    if(empty($title) || empty($descriere)){
+                        header("Location:../Gallery/gallery.php?upload=empty");
+                        exit();
+                    }else{
+                        $stmt=mysqli_stmt_init($conn);
+                        $sql="INSERT INTO coins(title,value,country,createdAt, description, imgFullName) VALUES (?,?,?,?,?,?)";
+                        if(!mysqli_stmt_prepare($stmt,$sql)){
+                            echo"SQL Error";
+                        }else{
+                            mysqli_stmt_bind_param($stmt,"sissss",$title,$valoare,$tara,$createdAt,$descriere,$imageFullName);
+                            mysqli_stmt_execute($stmt);
+                            // cand e uploadat, fisierul are o durata de viata temporara, il mutam intr un folder pentru a fi incarcat mai tarziu in galerie dintr-un select
+                            move_uploaded_file($fileTempName,$fileDestination);
+                            header("Location:../Gallery/gallery.php?upload=success");
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+?>
