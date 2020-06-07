3db13eb2e3a064a7fc70bafaf76dac9a108278cc
Stelian Loghin <alexandruloghinstelian121@yahoo.com>
2020-06-07 19:45:08 +0300

    my collection fix

--- Gallery/gallery.php
+++ Gallery/gallery.php
@@ -206,15 +206,14 @@ session_start();
                 if (isset($_GET['id'])) {
                     $id_moneda = $_GET['id'];
                     $id_user = $_SESSION['ID'];
-                    $azi = date("Y-m-d");
-                    $sql = "INSERT INTO inventory(id_user,id_coin,added_at) values(?,?,?);";
+                    $sql = "INSERT INTO inventory (id_user,id_coin) values(".$id_user.",".$id_moneda.");";
                     // echo '<p style="margin-left:30vw;color:red;font-size:50px;">'. $id_user . '</p>';
                     $stmt=mysqli_stmt_init($conn);
                     if (!mysqli_stmt_prepare($stmt, $sql)) {
                         header("Location:gallery.php?error=sqlerror");
                         exit();
                     }
-                    mysqli_stmt_bind_param($stmt,"iis",$id_user,$id_moneda,$azi);
+                    // mysqli_stmt_bind_param($stmt,"ii",$id_user,$id_moneda);
                     mysqli_stmt_execute($stmt);
                     echo '<p style="margin-left:30vw;color:red;font-size:50px;">'. $id_user . '</p>';
                     // header("Location:gallery.php?added=success");
